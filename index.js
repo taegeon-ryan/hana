bd9f2113495c8d0623570552efc34623d1a4189a
Hana <momenthana@kakao.com>
2020-01-13 20:13:27 +0900

    Send a message

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const { RTMClient } = require('@slack/rtm-api');
 const token = process.env.SLACK_BOT_TOKEN;
 const rtm = new RTMClient(token);
 
-rtm.on('message', (event) => {
-  console.log(event);
+rtm.on('member_joined_channel', async (event) => {
+  try {
+    const reply = await rtm.sendMessage(`안녕, 친구들! <@${event.user}> 등장!`, event.channel)
+    console.log('Message sent successfully', reply.ts);
+  } catch (error) {
+    console.log('An error occurred', error);
+  }
+});
+
+rtm.on('message', async (event) => {
+  try {
+    const reply = await rtm.sendMessage(event.text, event.channel)
+    console.log('Message sent successfully', reply.ts);
+  } catch (error) {
+    console.log('An error occurred', error);
+  }
 });
 
 (async () => {
