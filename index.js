7d8cdf91816171480de1a82ec1796e4776e4bbb6
Hana <momenthana@kakao.com>
2020-01-13 19:14:22 +0900

    Connect to Slack

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { RTMClient } = require('@slack/rtm-api');
-
 const token = process.env.SLACK_BOT_TOKEN;
 
 const rtm = new RTMClient(token);
+
+(async () => {
+  const { self, team } = await rtm.start();
+})();
