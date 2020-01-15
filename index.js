e7e3d0461a9fa4957489149312723c51e39ff703
Hana <momenthana@kakao.com>
2020-01-15 21:53:53 +0900

    사용자 입장 메시지 다중 적용

--- index.js
+++ index.js
@@ -8,9 +8,18 @@ const school = new School()
 
 school.init(eval('School.Type.' + process.env.SCHOOL_TYPE), eval('School.Region.' + process.env.SCHOOL_REGION), process.env.SCHOOL_CODE)
 
+const message = [
+  '`안녕, 친구들! <@${event.user}> 등장!`',
+  '`우린 당신을 기다리고 있었어요 <@${event.user}> 님`',
+  '`<@${event.user}> 님이 방금 서버에 참가하셨어요. 그럼 이제 게임을 즐겨보세요!`',
+  '`<@${event.user}> 님이 파티에 참가하셨어요.`',
+  '`저기요! 들어봐요! <@${event.user}> 님이 참가하셨어요!`'
+]
+
 rtm.on('member_joined_channel', async (event) => {
   try {
-    const reply = await rtm.sendMessage(`안녕, 친구들! <@${event.user}> 등장!`, event.channel)
+    random = Math.floor(Math.random() * message.length)
+    const reply = await rtm.sendMessage(eval(message[random]), event.channel)
     console.log('Message sent successfully', reply.ts)
   } catch (error) {
     console.log('An error occurred', error)
@@ -23,7 +32,8 @@ rtm.on('message', async (event) => {
 
     if (text.includes("급식")) {
       const meal = await school.getMeal({ default: '급식이 없습니다' })
-      reply = await rtm.sendMessage(meal.today, event.channel)
+      const reply = await rtm.sendMessage(meal.today, event.channel)
+      console.log('Message sent successfully', reply.ts)
     }
     
     if (text.includes("일정")) {
@@ -37,10 +47,9 @@ rtm.on('message', async (event) => {
         msg += `\n[${day}일] ${calendar[day]}`
       }
 
-      reply = await rtm.sendMessage(msg, event.channel)
+      const reply = await rtm.sendMessage(msg, event.channel)
+      console.log('Message sent successfully', reply.ts)
     }
-
-    console.log('Message sent successfully', reply.ts)
   } catch (error) {
     console.log('An error occurred', error)
   }
