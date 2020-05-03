8ee840a53b656e0b115965c44e281fc57145c8e7
Hana <momenthana@kakao.com>
2020-05-03 09:55:38 +0900

    이름 언급 추가

--- src/school.js
+++ src/school.js
@@ -121,84 +121,85 @@ const meal = async (school, date, type) => {
 
 const index = async (text, channel) => {
   let info
-
-  if (text.match(/검색/)) {
-    info = ''
-    const result = []
-    if (text.match(/.*(초|중|고|학교|유치원)/)) {
-      for (let key in School.Region) {
-        const search = await new School().search(School.Region[key], text.match(/.*(초|중|고|학교|유치원)/)[0])
-        search.forEach(e => {
-          let addr
-          for (const name in define.region) {
-            if (key === name) {
-              addr = define.region[name]
+  if (text.includes('하나')) {
+    if (text.match(/검색/)) {
+      info = ''
+      const result = []
+      if (text.match(/.*(초|중|고|학교|유치원)/)) {
+        for (let key in School.Region) {
+          const search = await new School().search(School.Region[key], text.match(/.*(초|중|고|학교|유치원)/)[0])
+          search.forEach(e => {
+            let addr
+            for (const name in define.region) {
+              if (key === name) {
+                addr = define.region[name]
+              }
             }
-          }
-          let type = 'HIGH'
-          let schoolExp = { '중학교': 'MIDDLE', '초등학교': 'ELEMENTARY', '유치원': 'KINDERGARTEN' }
-          for (let name in schoolExp) {
-            if (e.name.match(name)) {
-              type = schoolExp[name]
+            let type = 'HIGH'
+            let schoolExp = { '중학교': 'MIDDLE', '초등학교': 'ELEMENTARY', '유치원': 'KINDERGARTEN' }
+            for (let name in schoolExp) {
+              if (e.name.match(name)) {
+                type = schoolExp[name]
+              }
             }
+            result.push({ name: e.name, type: type, schoolCode: e.schoolCode, region: key, schoolRegion: addr, address: e.address })
+          })
+        }
+        for (const key in result) {
+          info += `\n${Number(key) + 1}. ${result[key].name} (${result[key].address !== ' ' ? result[key].address : result[key].schoolRegion ? result[key].schoolRegion + '교육청 소재' : '소재지 정보 없음'})`
+          search[channel] = result
+        }
+        info += `\n'1번 등록해줘'처럼 말해주면 채널에 등록해줄게`
+      } else {
+        info = '학교나 유치원 이름을 정확하게 입력해줘!'
+      }
+    }
+
+    if (text.match(/등록/)) {
+      const data = load()
+      if (!search[channel]) {
+        info = `채널에서 검색된 학교나 유치원이 없어!\n'하나고등학교 검색해줘'처럼 말해주면 내가 찾아줄게`
+      } else {
+        let i = search[channel][Number(text.replace(/[^{0-9}]/gi, '')) - 1]
+        info = `${i.name}${i.type == 'KINDERGARTEN' ? '을' : '를'} 채널에 등록했어!`
+        data[channel] = { type: i.type, region: i.region, schoolCode: i.schoolCode }
+      }
+      save(data)
+    }
+
+    const date = dateConvert(text)
+    const match = text.match(/(조식|중식|석식|급식)/)
+    if (match) {
+      const data = load()
+      if (!data[channel]) {
+        info = `채널에 등록된 학교나 유치원이 없어!\n'하나고등학교 검색해줘'처럼 말해주면 내가 찾아줄게`
+      } else {
+        let school = new School()
+        school.init(School.Type[data[channel].type], School.Region[data[channel].region], data[channel].schoolCode)
+        info = `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일 (${define.week[date.getDay()]})\n`
+        info += await meal(school, date, match[0])
+      }
+    }
+
+    if (text.includes('일정')) {
+      const data = load()
+      if (!data[channel]) {
+        info = `채널에 등록된 학교나 유치원이 없어!\n'하나고등학교 검색해줘'처럼 말해주면 내가 찾아줄게`
+      } else {
+        let school = new School()
+        school.init(School.Type[data[channel].type], School.Region[data[channel].region], data[channel].schoolCode)
+        const calendar = await school.getCalendar({ default: null })
+        info = `[${calendar.year}년 ${calendar.month}월]\n`
+
+        delete calendar.year
+        delete calendar.month
+        delete calendar.day
+        delete calendar.today
+
+        for (const day in calendar) {
+          if (calendar[day]) {
+            info += `[${day}일] ${calendar[day]}\n`
           }
-          result.push({ name: e.name, type: type, schoolCode: e.schoolCode, region: key, schoolRegion: addr, address: e.address })
-        })
-      }
-      for (const key in result) {
-        info += `\n${Number(key) + 1}. ${result[key].name} (${result[key].address !== ' ' ? result[key].address : result[key].schoolRegion ? result[key].schoolRegion + '교육청 소재' : '소재지 정보 없음'})`
-        search[channel] = result
-      }
-      info += `\n'1번 등록해줘'처럼 말해주면 채널에 등록해줄게`
-    } else {
-      info = '학교나 유치원 이름을 정확하게 입력해줘!'
-    }
-  }
-
-  if (text.match(/등록/)) {
-    const data = load()
-    if (!search[channel]) {
-      info = `채널에서 검색된 학교나 유치원이 없어!\n'하나고등학교 검색해줘'처럼 말해주면 내가 찾아줄게`
-    } else {
-      let i = search[channel][Number(text.replace(/[^{0-9}]/gi, '')) - 1]
-      info = `${i.name}${i.type == 'KINDERGARTEN' ? '을' : '를'} 채널에 등록했어!`
-      data[channel] = { type: i.type, region: i.region, schoolCode: i.schoolCode }
-    }
-    save(data)
-  }
-
-  const date = dateConvert(text)
-  const match = text.match(/(조식|중식|석식|급식)/)
-  if (match) {
-    const data = load()
-    if (!data[channel]) {
-      info = `채널에 등록된 학교나 유치원이 없어!\n'하나고등학교 검색해줘'처럼 말해주면 내가 찾아줄게`
-    } else {
-      let school = new School()
-      school.init(School.Type[data[channel].type], School.Region[data[channel].region], data[channel].schoolCode)
-      info = `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일 (${define.week[date.getDay()]})\n`
-      info += await meal(school, date, match[0])
-    }
-  }
-
-  if (text.includes('일정')) {
-    const data = load()
-    if (!data[channel]) {
-      info = `채널에 등록된 학교나 유치원이 없어!\n'하나고등학교 검색해줘'처럼 말해주면 내가 찾아줄게`
-    } else {
-      let school = new School()
-      school.init(School.Type[data[channel].type], School.Region[data[channel].region], data[channel].schoolCode)
-      const calendar = await school.getCalendar({ default: null })
-      info = `[${calendar.year}년 ${calendar.month}월]\n`
-
-      delete calendar.year
-      delete calendar.month
-      delete calendar.day
-      delete calendar.today
-
-      for (const day in calendar) {
-        if (calendar[day]) {
-          info += `[${day}일] ${calendar[day]}\n`
         }
       }
     }
