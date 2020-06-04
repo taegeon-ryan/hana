788c1d99de7989b970a042e151879e4d071ba447
Hana <momenthana@kakao.com>
2020-06-04 15:11:34 +0900

    Docs config 수정

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,20 @@
 module.exports = {
   title: 'Hana',
   description: '',
-  base: '/hana'
+  base: '/hana',
+  themeConfig: {
+    nav: [
+      { text: 'Index', link: '/' }
+    ]
+  },
+  plugins: [
+    '@vuepress/back-to-top',
+    '@vuepress/active-header-links',
+    '@vuepress/medium-zoom',
+    '@vuepress/nprogress',
+    ['@vuepress/pwa', {
+      serviceWorker: true,
+      updatePopup: true
+    }]
+  ]
 }
